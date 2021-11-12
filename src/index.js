1fa3e3ba96283a469477df1fadc32966e6258d15
Tom Costa <tomcosta@hotmail.co.uk>
2021-11-12 12:22:19 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Heading from './components/Heading'
+import List from './components/List'
+import App from './components/App'
+
+ReactDOM.render(<App />, document.getElementById('root'))
